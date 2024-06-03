0c471ce701ea60d7ce31de83bd0564535ca913e3
ravi <ravi@localhost.domain>
2024-06-03 17:26:59 +0530

    updated for github action demo

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Devops Training , How are you. password is Abc#23@we !" );
+    }
+}
